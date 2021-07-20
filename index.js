94b31f288722f3c6745c92ce198027ea8fd1d04f
Lucas Nunes <lucaszero12@gmail.com>
2021-07-20 00:11:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,168 @@
+Vue.directive("mask", VueTheMask.mask);
+var app = new Vue({
+  el: "#id",
+  data: {
+    tela: 0,
+    lista: [],
+    nome: "",
+    email: "",
+    cpf: "",
+    telefone: "",
+    cep: "",
+    logradouro: "",
+    numero: "",
+    bairro: "",
+    cidade: "",
+    estado: "",
+    alert: false,
+    controler: "",
+  },
+  created() {
+    var key = localStorage.getItem("key");
+    if (key != null) {
+      this.lista = JSON.parse(key);
+    }
+  },
+  methods: {
+    deletar(index) {
+      this.lista.splice(index, 1);
+      localStorage.setItem("key", JSON.stringify(this.lista));
+    },
+    mudartela(tela) {
+      this.alert = false;
+      this.tela = tela;
+      this.controler = "";
+    },
+
+    salvar() {
+      this.controler = "all";
+      if (
+        !this.regrasnome &&
+        !this.regrasemail &&
+        !this.regrasestado &&
+        !this.regraslogradouro &&
+        !this.regrasnumero &&
+        !this.regrastelefone &&
+        !this.regrasbairro &&
+        !this.regrascep &&
+        !this.regrascpf
+      ) {
+        var dados = {
+          nome: this.nome,
+          email: this.email,
+          cpf: this.cpf,
+          telefone: this.telefone,
+          cep: this.cep,
+          logradouro: this.logradouro,
+          numero: this.numero,
+          bairro: this.bairro,
+          cidade: this.cidade,
+          estado: this.estado,
+        };
+        this.alert = true;
+        this.lista.push(dados);
+        localStorage.setItem("key", JSON.stringify(this.lista));
+        (this.nome = ""),
+          (this.email = ""),
+          (this.cpf = ""),
+          (this.telefone = ""),
+          (this.cep = ""),
+          (this.logradouro = ""),
+          (this.numero = ""),
+          (this.bairro = ""),
+          (this.cidade = ""),
+          (this.estado = "");
+        this.controler = "";
+      }
+    },
+  },
+  computed: {
+    regrasnome() {
+      if (this.controler == "nome" || this.controler == "all") {
+        if (this.nome == "") {
+          return "Nome Invalido";
+        }
+        return false;
+      }
+      return false;
+    },
+    regrasemail() {
+      if (this.controler == "email" || this.controler == "all") {
+        var re = /\S+@\S+\.\S+/;
+        if (!re.test(this.email)) return "Email invalido";
+        return false;
+      }
+      return false;
+    },
+    regrascpf() {
+      if (this.controler == "cpf" || this.controler == "all") {
+        var re =
+          /^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}|[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/;
+        if (!re.test(this.cpf)) return "CPF?CNPJ invalido";
+        return false;
+      }
+      return false;
+    },
+    regrastelefone() {
+      if (this.controler == "telefone" || this.controler == "all") {
+        var re = /(\(\d{2}\)\s)(\d{4,5}\-\d{4})/g;
+        if (!re.test(this.telefone)) return "Telefone invalido";
+        return false;
+      }
+      return false;
+    },
+    regrascep() {
+      if (this.controler == "cep" || this.controler == "all") {
+        var re = /^[0-9]{2}.[0-9]{3}-[0-9]{3}$/;
+        if (!re.test(this.cep)) return "CEP invalido";
+        return false;
+      }
+      return false;
+    },
+    regraslogradouro() {
+      if (this.controler == "logradouro" || this.controler == "all") {
+        if (this.logradouro == "") {
+          return "logradouro Invalido";
+        }
+        return false;
+      }
+      return false;
+    },
+    regrasnumero() {
+      if (this.controler == "numero" || this.controler == "all") {
+        if (this.numero == "" || this.numero < 0) {
+          return "numero Invalido";
+        }
+        return false;
+      }
+      return false;
+    },
+    regrasbairro() {
+      if (this.controler == "bairro" || this.controler == "all") {
+        if (this.bairro == "") {
+          return "bairro Invalido";
+        }
+        return false;
+      }
+      return false;
+    },
+    regrascidade() {
+      if (this.controler == "cidade" || this.controler == "all") {
+        if (this.cidade == "") {
+          return "cidade Invalido";
+        }
+        return false;
+      }
+      return false;
+    },
+    regrasestado() {
+      if (this.controler == "estado" || this.controler == "all") {
+        if (this.estado == "") {
+          return "estado Invalido";
+        }
+        return false;
+      }
+      return false;
+    },
+  },
+});
